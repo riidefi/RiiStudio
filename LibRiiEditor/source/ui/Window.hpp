d943317b49eba3436ef6ffa30dd1ccf24783e466
riidefi <34194588+riidefi@users.noreply.github.com>
2019-09-07 01:17:04 -0400

    :sparkles: Completed WindowContext, adding a basic core resource interface.

--- LibRiiEditor/source/ui/Window.hpp
+++ LibRiiEditor/source/ui/Window.hpp
@@ -4,17 +4,29 @@
 
 struct SelectionManager;
 
-struct TODO
-{};
 
+//! @brief		The core resource describes the root data (scene, archive, etc.) that an applet holds.
+//!
+//! @remarks	Management of the data itself is deferred to the applet implementation, requiring a potentially unsafe cast
+//!				that should be safely encapsulated.
+//!
+struct CoreResource
+{
+	virtual ~CoreResource() = default;
+};
+
+//! @brief An (optional) context passed to windows when drawing.
+//!
+//! @remarks The core resource reference is non-owning -- the applet must not delete it while dispatching windows.
+//!
 struct WindowContext
 {
-	WindowContext(SelectionManager& mgr, TODO& res)
+	WindowContext(SelectionManager& mgr, CoreResource& res)
 		: selectionManager(mgr), core_resource(res){}
 	~WindowContext() = default;
 
 	SelectionManager& selectionManager;
-	TODO& core_resource;
+	CoreResource& core_resource;
 };
 
 struct Window
@@ -29,6 +41,6 @@ struct Window
 	//!
 	virtual void draw(WindowContext* ctx = nullptr) noexcept = 0;
 
-	bool bOpen = true;
+	bool bOpen = true; //!< If the window is opened, for ImGui. When false, window manager will destroy the window itself.
 	u32 mId; //!< ID, assigned by window manager, do not modify
 };
