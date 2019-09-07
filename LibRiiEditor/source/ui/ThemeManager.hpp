124ba22bbcf718248c1cb6f4e6c095ae8c358a5e
riidefi <34194588+riidefi@users.noreply.github.com>
2019-09-06 21:54:54 -0400

    :sparkles: Added an adaptive style.

--- LibRiiEditor/source/ui/ThemeManager.hpp
+++ LibRiiEditor/source/ui/ThemeManager.hpp
@@ -1,62 +1,68 @@
 #pragma once
 
 #include <imgui/imgui.h>
+#include <cmath>
 
-namespace ThemeManager {
-
-enum class Theme
+class ThemeManager
 {
-	ImClassic,
-	ImDark,
-	ImLight,
-	UE4ish,
+public:
+#pragma region Basic Themes
 
-	Default = UE4ish
-};
-
-template<Theme theme>
-inline void setTheme()
-{}
-
-inline void setTheme(Theme theme)
-{
-	switch (theme)
+	enum class BasicTheme
 	{
-	case Theme::ImClassic:
-		setTheme<Theme::ImClassic>();
-		break;
-	case Theme::ImDark:
-		setTheme<Theme::ImDark>();
-		break;
-	case Theme::ImLight:
-		setTheme<Theme::ImLight>();
-		break;
-	case Theme::UE4ish:
-		setTheme<Theme::UE4ish>();
-		break;
+		// Basic Themes
+		ImClassic,
+		ImDark,
+		ImLight,
+		UE4ish,
+
+		Default = UE4ish
+	};
+
+
+	template<BasicTheme theme>
+	static inline void setTheme()
+	{}
+
+	static inline void setTheme(BasicTheme theme)
+	{
+		switch (theme)
+		{
+		case BasicTheme::ImClassic:
+			setTheme<BasicTheme::ImClassic>();
+			break;
+		case BasicTheme::ImDark:
+			setTheme<BasicTheme::ImDark>();
+			break;
+		case BasicTheme::ImLight:
+			setTheme<BasicTheme::ImLight>();
+			break;
+		case BasicTheme::UE4ish:
+			setTheme<BasicTheme::UE4ish>();
+			break;
+		}
 	}
-}
 
-template<>
-inline void setTheme<Theme::ImClassic>()
-{
-	ImGui::StyleColorsClassic();
-}
+	template<>
+	static inline void setTheme<BasicTheme::ImClassic>()
+	{
+		ImGui::StyleColorsClassic();
+	}
 
-template<>
-inline void setTheme<Theme::ImDark>()
-{
-	ImGui::StyleColorsDark();
-}
+	template<>
+	static inline void setTheme<BasicTheme::ImDark>()
+	{
+		ImGui::StyleColorsDark();
+	}
 
-template<>
-inline void setTheme<Theme::ImLight>()
-{
-	ImGui::StyleColorsLight();
-}
+	template<>
+	static inline void setTheme<BasicTheme::ImLight>()
+	{
+		ImGui::StyleColorsLight();
+	}
 
-template<>
-inline void setTheme<Theme::UE4ish>()
+	template<>
+	static inline void setTheme<BasicTheme::UE4ish>()
 	{
 		// https://github.com/ocornut/imgui/issues/707#issuecomment-415097227
 		ImVec4* colors = ImGui::GetStyle().Colors;
@@ -104,4 +110,112 @@ inline void setTheme<Theme::UE4ish>()
 		colors[ImGuiCol_NavWindowingHighlight] = ImVec4(1.00f, 1.00f, 1.00f, 0.70f);
 	}
 
-} // namespce ThemeManager
+#pragma endregion
+
+#pragma region Adaptive Theme
+	// Based on https://github.com/ocornut/imgui/issues/707#issuecomment-508691523
+
+	enum Colors
+	{
+		Black = 0x00000000,
+		White = 0xFFFFFF00,
+		AlphaTransparent = 0x00,
+		Alpha20 = 0x33,
+		Alpha40 = 0x66,
+		Alpha50 = 0x80,
+		Alpha60 = 0x99,
+		Alpha80 = 0xCC,
+		Alpha90 = 0xE6,
+		AlphaFull = 0xFF
+	};
+
+	int BackGroundColor = 0x25213100;
+	int TextColor = 0xF4F1DE00;
+	int MainColor = 0xDA115E00;
+	int MainAccentColor = 0x79235900;
+	int HighlightColor = 0xC7EF0000;
+
+	static constexpr float GetR(int colorCode) { return (float)((colorCode & 0xFF000000) >> 24) / (float)(0xFF); }
+	static constexpr float GetG(int colorCode) { return (float)((colorCode & 0x00FF0000) >> 16) / (float)(0xFF); }
+	static constexpr float GetB(int colorCode) { return (float)((colorCode & 0x0000FF00) >> 8) / (float)(0xFF); }
+	static constexpr float GetA(int alphaCode) { return ((float)alphaCode / (float)0xFF); }
+
+	// TODO: Make this all constexpr
+
+	static ImVec4 GetColor(int c, int a = Alpha80) { return ImVec4(GetR(c), GetG(c), GetB(c), GetA(a)); }
+	static ImVec4 Darken(ImVec4 c, float p) { return ImVec4(fmax(0.f, c.x - 1.0f * p), fmax(0.f, c.y - 1.0f * p), fmax(0.f, c.z - 1.0f *p), c.w); }
+	static ImVec4 Lighten(ImVec4 c, float p) { return ImVec4(fmax(0.f, c.x + 1.0f * p), fmax(0.f, c.y + 1.0f * p), fmax(0.f, c.z + 1.0f *p), c.w); }
+
+	static ImVec4 Disabled(ImVec4 c) { return Darken(c, 0.6f); }
+	static ImVec4 Hovered(ImVec4 c) { return Lighten(c, 0.2f); }
+	static ImVec4 Active(ImVec4 c) { return Lighten(ImVec4(c.x, c.y, c.z, 1.0f), 0.1f); }
+	static ImVec4 Collapsed(ImVec4 c) { return Darken(c, 0.2f); }
+
+	inline void SetColors(int backGroundColor, int textColor, int mainColor, int mainAccentColor, int highlightColor)
+	{
+		BackGroundColor = backGroundColor;
+		TextColor = textColor;
+		MainColor = mainColor;
+		MainAccentColor = mainAccentColor;
+		HighlightColor = highlightColor;
+	}
+
+	inline void setThemeAdaptive()
+	{
+		ImVec4* colors = ImGui::GetStyle().Colors;
+
+		colors[ImGuiCol_Text] = GetColor(TextColor);
+		colors[ImGuiCol_TextDisabled] = Disabled(colors[ImGuiCol_Text]);
+		colors[ImGuiCol_WindowBg] = GetColor(BackGroundColor);
+		colors[ImGuiCol_ChildBg] = GetColor(Black, Alpha20);
+		colors[ImGuiCol_PopupBg] = GetColor(BackGroundColor, Alpha90);
+		colors[ImGuiCol_Border] = Lighten(GetColor(BackGroundColor), 0.4f);
+		colors[ImGuiCol_BorderShadow] = GetColor(Black);
+		colors[ImGuiCol_FrameBg] = GetColor(MainAccentColor, Alpha40);
+		colors[ImGuiCol_FrameBgHovered] = Hovered(colors[ImGuiCol_FrameBg]);
+		colors[ImGuiCol_FrameBgActive] = Active(colors[ImGuiCol_FrameBg]);
+		colors[ImGuiCol_TitleBg] = GetColor(BackGroundColor, Alpha80);
+		colors[ImGuiCol_TitleBgActive] = Active(colors[ImGuiCol_TitleBg]);
+		colors[ImGuiCol_TitleBgCollapsed] = Collapsed(colors[ImGuiCol_TitleBg]);
+		colors[ImGuiCol_MenuBarBg] = Darken(GetColor(BackGroundColor), 0.2f);
+		colors[ImGuiCol_ScrollbarBg] = Lighten(GetColor(BackGroundColor, Alpha50), 0.4f);
+		colors[ImGuiCol_ScrollbarGrab] = Lighten(GetColor(BackGroundColor), 0.3f);
+		colors[ImGuiCol_ScrollbarGrabHovered] = Hovered(colors[ImGuiCol_ScrollbarGrab]);
+		colors[ImGuiCol_ScrollbarGrabActive] = Active(colors[ImGuiCol_ScrollbarGrab]);
+		colors[ImGuiCol_CheckMark] = GetColor(HighlightColor);
+		colors[ImGuiCol_SliderGrab] = GetColor(HighlightColor);
+		colors[ImGuiCol_SliderGrabActive] = Active(colors[ImGuiCol_SliderGrab]);
+		colors[ImGuiCol_Button] = GetColor(MainColor, Alpha80);
+		colors[ImGuiCol_ButtonHovered] = Hovered(colors[ImGuiCol_Button]);
+		colors[ImGuiCol_ButtonActive] = Active(colors[ImGuiCol_Button]);
+		colors[ImGuiCol_Header] = GetColor(MainAccentColor, Alpha80);
+		colors[ImGuiCol_HeaderHovered] = Hovered(colors[ImGuiCol_Header]);
+		colors[ImGuiCol_HeaderActive] = Active(colors[ImGuiCol_Header]);
+		colors[ImGuiCol_Separator] = colors[ImGuiCol_Border];
+		colors[ImGuiCol_SeparatorHovered] = Hovered(colors[ImGuiCol_Separator]);
+		colors[ImGuiCol_SeparatorActive] = Active(colors[ImGuiCol_Separator]);
+		colors[ImGuiCol_ResizeGrip] = GetColor(MainColor, Alpha20);
+		colors[ImGuiCol_ResizeGripHovered] = Hovered(colors[ImGuiCol_ResizeGrip]);
+		colors[ImGuiCol_ResizeGripActive] = Active(colors[ImGuiCol_ResizeGrip]);
+		colors[ImGuiCol_Tab] = GetColor(MainColor, Alpha60);
+		colors[ImGuiCol_TabHovered] = Hovered(colors[ImGuiCol_Tab]);
+		colors[ImGuiCol_TabActive] = Active(colors[ImGuiCol_Tab]);
+		colors[ImGuiCol_TabUnfocused] = colors[ImGuiCol_Tab];
+		colors[ImGuiCol_TabUnfocusedActive] = colors[ImGuiCol_TabActive];
+		colors[ImGuiCol_DockingPreview] = Darken(colors[ImGuiCol_HeaderActive], 0.2f);
+		colors[ImGuiCol_DockingEmptyBg] = Darken(colors[ImGuiCol_HeaderActive], 0.6f);
+		colors[ImGuiCol_PlotLines] = GetColor(HighlightColor);
+		colors[ImGuiCol_PlotLinesHovered] = Hovered(colors[ImGuiCol_PlotLines]);
+		colors[ImGuiCol_PlotHistogram] = GetColor(HighlightColor);
+		colors[ImGuiCol_PlotHistogramHovered] = Hovered(colors[ImGuiCol_PlotHistogram]);
+		colors[ImGuiCol_TextSelectedBg] = GetColor(HighlightColor, Alpha40);
+		colors[ImGuiCol_DragDropTarget] = GetColor(HighlightColor, Alpha80);;
+		colors[ImGuiCol_NavHighlight] = GetColor(White);
+		colors[ImGuiCol_NavWindowingHighlight] = GetColor(White, Alpha80);
+		colors[ImGuiCol_NavWindowingDimBg] = GetColor(White, Alpha20);
+		colors[ImGuiCol_ModalWindowDimBg] = GetColor(Black, Alpha60);
+
+		ImGui::GetStyle().WindowMenuButtonPosition = ImGuiDir_Right;
+	}
+#pragma endregion
+};
