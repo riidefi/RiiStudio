c4de80e6e86c9c62c257640747a8c3e68d7cecfe
riidefi <34194588+riidefi@users.noreply.github.com>
2019-09-07 01:06:31 -0400

    :sparkles: Added applet base.

---
+++ LibRiiEditor/source/core/Applet.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "WindowManager.hpp"
+#include "GL_Window.hpp"
+
+class Applet : public WindowManager, public GL_Window
+{
+public:
+	Applet();
+	~Applet();
+
+	void frameProcess() override;
+	void frameRender() override;
+
+	virtual void drawRoot() {}
+	virtual WindowContext makeWindowContext() = 0;
+};
