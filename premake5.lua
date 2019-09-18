bce6fc1f17a5053a1d67165bb01f6d33dd0e84b3
riidefi <34194588+riidefi@users.noreply.github.com>
2019-09-17 23:35:26 -0400

    :construction: Build system refactor + Game format library project added.

--- premake5.lua
+++ premake5.lua
@@ -13,13 +13,35 @@ workspace "RiiStudio"
 		"MultiProcessorCompile"
 	}
 
+function setupPreprocessor()
+	filter "configurations:Debug"
+		defines "DEBUG"
+		symbols "On"
+	
+	filter "configurations:Release"
+		defines "NDEBUG"
+		optimize "On"
+	
+	filter "configurations:Dist"
+		defines "NDEBUG"
+		optimize "On"
+end
+
+function setupSystem()
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"RII_PLATFORM_WINDOWS"
+		}
+end
+
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
-project "LibRiiEditor"
-	location "LibRiiEditor"
-	kind "StaticLib"
-	language "C++"
-
+function setupCppC()
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 	
@@ -30,6 +52,85 @@ project "LibRiiEditor"
 		"%{prj.name}/source/**.cpp",
 		"%{prj.name}/source/**.c"
 	}
+
+	filter "files:*.cpp"
+		language "C++"
+	filter "files:*.c"
+		language "C"
+end
+
+function setupStaticLib()
+	kind "StaticLib"
+	defines "_LIB"
+end
+
+function setupConsoleApp()
+	kind "ConsoleApp"
+end
+
+
+project "LibRiiEditor"
+	location "LibRiiEditor"
+	setupStaticLib()
+	setupCppC()
+
+	includedirs {
+		"LibRiiEditor/source",
+		"ThirdParty/source",
+		"oishii"
+	}
+
+	setupSystem()
+	setupPreprocessor()
+
+project "TestEditor"
+	location "TestEditor"
+	setupConsoleApp()
+
+	setupCppC()
+
+	includedirs {
+		"LibRiiEditor/source",
+		"ThirdParty/source",
+		"oishii"
+	}
+
+	links
+	{
+		"LibRiiEditor",
+		"ThirdParty",
+		"LibCube",
+		"ThirdParty/glfw/lib-vc2017/glfw3dll.lib",
+		"opengl32.lib"
+	}
+	setupSystem()
+
+	postbuildcommands {
+  		--"{COPY} ThirdParty/glfw/lib-vc2017glfw3.dll %{cfg.targetdir}"
+	}
+
+	
+	setupPreprocessor()
+
+project "ThirdParty"
+	location "ThirdParty"
+	setupStaticLib()
+
+	setupCppC()
+
+	includedirs
+	{
+		"oishii",
+		"ThirdParty/source/"
+	}
+
+	setupSystem()
+	setupPreprocessor()
+
+project "LibCube"
+	location "LibCube"
+	setupStaticLib()
+	setupCppC()
 
 	includedirs
 	{
@@ -38,133 +139,5 @@ project "LibRiiEditor"
 		"oishii"
 	}
 
-	filter "system:windows"
-		cppdialect "C++17"
-		staticruntime "On"
-		systemversion "latest"
-
-		defines
-		{
-			"RII_PLATFORM_WINDOWS",
-			"_LIB"
-		}
-
-	filter "configurations:Debug"
-		defines "DEBUG"
-		symbols "On"
-	
-	filter "configurations:Release"
-		defines "NDEBUG"
-		optimize "On"
-	
-	filter "configurations:Dist"
-		defines "NDEBUG"
-		optimize "On"
-
-project "TestEditor"
-	location "TestEditor"
-	kind "ConsoleApp"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	files
-	{
-		"%{prj.name}/source/**.h",
-		"%{prj.name}/source/**.hpp",
-		"%{prj.name}/source/**.cpp",
-		"%{prj.name}/source/**.c"
-	}
-
-	includedirs
-	{
-		"LibRiiEditor/source",
-		"ThirdParty/source",
-		"oishii",
-	}
-
-	
-
-	defines
-	{
-		"RII_PLATFORM_WINDOWS",
-		"_LIB"
-	}
-	links
-	{
-		"LibRiiEditor",
-		"ThirdParty",
-		"ThirdParty/glfw/lib-vc2017/glfw3dll.lib",
-		"opengl32.lib"
-	}
-	filter "system:windows"
-		cppdialect "C++17"
-		staticruntime "On"
-		systemversion "latest"
-
-	postbuildcommands {
-  		--"{COPY} ThirdParty/glfw/lib-vc2017glfw3.dll %{cfg.targetdir}"
-	}
-
-	filter "files:*.cpp"
-		language "C++"
-	filter "files:*.c"
-		language "C"
-
-	filter "configurations:Debug"
-		defines "DEBUG"
-		symbols "On"
-	
-	filter "configurations:Release"
-		defines "NDEBUG"
-		optimize "On"
-	
-	filter "configurations:Dist"
-		defines "NDEBUG"
-		optimize "On"
-
-project "ThirdParty"
-	location "ThirdParty"
-	kind "StaticLib"
-	language "C++"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-	
-	files
-	{
-		"%{prj.name}/source/**.h",
-		"%{prj.name}/source/**.hpp",
-		"%{prj.name}/source/**.cpp",
-		"%{prj.name}/source/**.c"
-	}
-
-	includedirs
-	{
-		"oishii",
-		"ThirdParty/source/"
-	}
-
-	filter "system:windows"
-		cppdialect "C++17"
-		staticruntime "On"
-		systemversion "latest"
-
-		defines
-		{
-			"RII_PLATFORM_WINDOWS",
-			"_LIB"
-		}
-
-	filter "configurations:Debug"
-		defines "DEBUG"
-		symbols "On"
-	
-	filter "configurations:Release"
-		defines "NDEBUG"
-		optimize "On"
-	
-	filter "configurations:Dist"
-		defines "NDEBUG"
-		optimize "On"
-
+	setupSystem()
+	setupPreprocessor()
