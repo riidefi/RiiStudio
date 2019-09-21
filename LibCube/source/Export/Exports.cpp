1bb101934da7017249144296908efd14f2102930
riidefi <34194588+riidefi@users.noreply.github.com>
2019-09-21 18:58:23 -0400

    :recycle: Refactored TPL Editor.

--- LibCube/source/Export/Exports.cpp
+++ LibCube/source/Export/Exports.cpp
@@ -8,71 +8,54 @@ namespace libcube {
 
 // Static initialization unfortunate
 
-struct TPLEditor : public pl::FileEditor
+struct TPLEditor : public pl::FileEditor, public pl::Readable, public pl::ITextureList, public pl::TransformStack
 {
-	struct TODOInterface : public pl::TransformStack
+	// public pl::TransformStack
+	struct GenStats : public pl::TransformStack::XForm
 	{
-		struct GenStats : public pl::TransformStack::XForm
+		GenStats()
 		{
-			GenStats()
-			{
-				name = pl::RichName{"Generate Statistics", "GenStats", "stats"};
-				mParams.push_back(pl::TransformStack::Param{
-					pl::RichName{"Verbose", "verbose", "verbose"},
-					pl::TransformStack::ParamType::Flag,
-					{}});
-			}
-
-			void perform() override
-			{
-				printf("TODO: Generate stats!\n");
-			}
-		};
-
-		TODOInterface()
-		{
-			//mStack.emplace_back(XForm)
+			name = pl::RichName{"Generate Statistics", "GenStats", "stats"};
+			mParams.push_back(pl::TransformStack::Param{
+				pl::RichName{"Verbose", "verbose", "verbose"},
+				pl::TransformStack::ParamType::Flag,
+				{}});
 		}
 
+		void perform() override
+		{
+			printf("TODO: Generate stats!\n");
+		}
+	};
 
-	} xFormStack;
-
-	struct ReadTPL : public pl::Readable
+	// public pl::Readable
+	bool tryRead(oishii::BinaryReader& reader, FileEditor& ctx) override
 	{
-		bool tryRead(oishii::BinaryReader& reader, FileEditor& ctx) override
-		{
-			// Specialize context
-			TPLEditor& editor = static_cast<TPLEditor&>(ctx);
+		// Specialize context
+		TPLEditor& editor = static_cast<TPLEditor&>(ctx);
 
-			reader.dispatch<DolphinTPL>(editor.coreRes);
-			return true;
-		}
-
-	} readTpl;
-
-	struct TextureList : public pl::ITextureList
+		reader.dispatch<DolphinTPL>(editor.coreRes);
+		return true;
+	}
+	// public pl::ITextureList
+	u32 getNumTex(const pl::FileEditor& ctx) const override
 	{
-		u32 getNumTex(const pl::FileEditor& ctx) const override
-		{
-			const TPLEditor& editor = static_cast<const TPLEditor&>(ctx);
+		return coreRes.mTextures.size();
+	}
+	std::string getNameAt(const FileEditor& ctx, int idx) const override
+	{
+		const TPLEditor& editor = static_cast<const TPLEditor&>(ctx);
 
-			return editor.coreRes.mTextures.size();
-		}
-		std::string getNameAt(const FileEditor& ctx, int idx) const override
-		{
-			const TPLEditor& editor = static_cast<const TPLEditor&>(ctx);
-
-			return std::string("Texture #") + std::to_string(idx);
-		}
-	} texList;
-
+		return std::string("Texture #") + std::to_string(idx);
+	}
+	
 	TPLEditor()
 	{
 		mMagics.push_back(0x0020AF30);
 		mExtensions.push_back(".tpl");
-		mInterfaces.push_back(&xFormStack);
-		mInterfaces.push_back(&readTpl);
-		mInterfaces.push_back(&texList);
+		mInterfaces.push_back((pl::TransformStack*)this);
+		mInterfaces.push_back((pl::Readable*)this);
+		mInterfaces.push_back((pl::ITextureList*)this);
 	}
 
 private:
