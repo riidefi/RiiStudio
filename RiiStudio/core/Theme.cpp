8953fd7154997d39fdbfd741371d3585f78437d4
riidefi <34194588+riidefi@users.noreply.github.com>
2019-10-06 17:46:28 -0400

    :truck: Moved theme implementation out of header.

---
+++ RiiStudio/core/Theme.cpp
@@ -0,0 +1,40 @@
+#include "Theme.hpp"
+static inline int toIntComp(float src)
+{
+	return static_cast<int>(round(src * 255.0f));
+}
+static inline int toIntColor(const ImVec4& src)
+{
+	return	(toIntComp(src.x) << 24) |
+		(toIntComp(src.y) << 16) |
+		(toIntComp(src.z) << 8) |
+		(toIntComp(src.w));
+}
+void Theme::Editor::windowDraw(CoreContext& ctx) noexcept
+{
+	auto& manager = ctx.core_resource.theme;
+
+	ImGui::Combo("Theme", (int*)&manager.mThemeSelection, manager.mThemeManager.ThemeNames);
+
+	if (manager.mThemeSelection == ThemeManager::BasicTheme::Adaptive)
+	{
+		ImVec4 bgColor = manager.mThemeManager.GetColor(manager.mThemeManager.BackGroundColor);
+		ImVec4 txtColor = manager.mThemeManager.GetColor(manager.mThemeManager.TextColor);
+		ImVec4 mainColor = manager.mThemeManager.GetColor(manager.mThemeManager.MainColor);
+		ImVec4 accentColor = manager.mThemeManager.GetColor(manager.mThemeManager.MainAccentColor);
+		ImVec4 highlightColor = manager.mThemeManager.GetColor(manager.mThemeManager.HighlightColor);
+
+		ImGui::ColorEdit3("Background Color", &bgColor.x);
+		ImGui::ColorEdit3("Text Color", &txtColor.x);
+		ImGui::ColorEdit3("Main Color", &mainColor.x);
+		ImGui::ColorEdit3("Accent Color", &accentColor.x);
+		ImGui::ColorEdit3("Highlight Color", &highlightColor.x);
+
+		manager.mThemeManager.SetColors(toIntColor(bgColor),
+			toIntColor(txtColor),
+			toIntColor(mainColor),
+			toIntColor(accentColor),
+			toIntColor(highlightColor));
+	}
+	manager.mThemeManager.setThemeEx(manager.mThemeSelection);
+}
