0dad3b6b11093219fc6329cf6748b28a5cd08c42
riidefi <34194588+riidefi@users.noreply.github.com>
2020-12-30 04:31:31 -0500

    :truck: Moved array_vector to util

--- source/plugins/gc/Export/Material.hpp
+++ source/plugins/gc/Export/Material.hpp
@@ -1,86 +1,22 @@
 #pragma once
 
-#include "PropertySupport.hpp"
+#include "Texture.hpp"
 #include <core/3d/i3dmodel.hpp>
 #include <core/common.h>
-#include <lib_rii/gx.h>
-
 #include <core/kpi/Node.hpp>
-
-#include "Texture.hpp"
+#include <core/util/array_vector.hpp>
+#include <lib_rii/gx.h>
 
 namespace libcube {
 
+template <typename T, size_t N>
+using array_vector = riistudio::util::array_vector<T, N>;
+template <typename T, size_t N>
+using copyable_polymorphic_array_vector =
+    riistudio::util::copyable_polymorphic_array_vector<T, N>;
+
 class Model;
 
-// Assumption: all elements are contiguous--no holes
-// Much faster than a vector for the many static sized arrays in materials
-template <typename T, size_t N> struct array_vector : public std::array<T, N> {
-  size_t size() const { return nElements; }
-  size_t nElements = 0;
-
-  void push_back(T elem) {
-    std::array<T, N>::at(nElements) = std::move(elem);
-    ++nElements;
-  }
-  void pop_back() { --nElements; }
-  bool operator==(const array_vector& rhs) const noexcept {
-    if (rhs.nElements != nElements)
-      return false;
-    for (int i = 0; i < nElements; ++i) {
-      const T& l = this->at(i);
-      const T& r = rhs.at(i);
-      if (!(l == r))
-        return false;
-    }
-    return true;
-  }
-  void erase(std::size_t index) {
-    assert(index >= 0 && index < nElements);
-    for (std::size_t i = index; i < nElements - 1; ++i) {
-      std::array<T, N>::at(i) = std::move(std::array<T, N>::at(i + 1));
-    }
-    --nElements;
-  }
-};
-
-template <typename T, std::size_t size>
-struct copyable_polymorphic_array_vector
-    : public array_vector<std::unique_ptr<T>, size> {
-#ifdef _MSVC_VER
-  using super = array_vector;
-#else
-  // MSVC bug?
-  using super = array_vector<std::unique_ptr<T>, size>;
-#endif
-  copyable_polymorphic_array_vector&
-  operator=(const copyable_polymorphic_array_vector& rhs) {
-    super::nElements = rhs.super::nElements;
-    for (std::size_t i = 0; i < super::nElements; ++i) {
-      super::at(i) = nullptr;
-    }
-    for (std::size_t i = 0; i < rhs.super::nElements; ++i) {
-      super::at(i) = rhs.super::at(i)->clone();
-    }
-    return *this;
-  }
-  bool operator==(const copyable_polymorphic_array_vector& rhs) const noexcept {
-    if (rhs.nElements != super::nElements)
-      return false;
-    for (int i = 0; i < super::nElements; ++i) {
-      const T& l = *this->at(i).get();
-      const T& r = *rhs.at(i).get();
-      if (!(l == r))
-        return false;
-    }
-    return true;
-  }
-  copyable_polymorphic_array_vector(
-      const copyable_polymorphic_array_vector& rhs) {
-    *this = rhs;
-  }
-  copyable_polymorphic_array_vector() = default;
-};
 struct GCMaterialData {
   // TODO:
   bool operator==(const GCMaterialData& rhs) const { return false; }
