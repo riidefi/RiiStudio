1cbac10720ceeffda597574d3e0e332d29bf598a
riidefi <34194588+riidefi@users.noreply.github.com>
2021-01-10 14:49:17 -0500

    :construction: tests.py stub

---
+++ tests/tests.py
@@ -0,0 +1,14 @@
+'''
+The testing suite is results-driven:
+We simply check: Can all field artifacts be rebuilt losslessly?
+'''
+
+TESTS_EXEC = "TODO"
+
+def rebuild(input_path, output_path):
+	'''
+	Rebuild a file
+	Throw an error if the program faults, returning the stacktrace.
+	'''
+	pass
+
