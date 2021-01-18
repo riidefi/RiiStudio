cb7265fb11d8398d7bfbb92f89b7056a366a7bd3
riidefi <34194588+riidefi@users.noreply.github.com>
2021-01-17 17:10:20 -0700

    :bug: Fix material name being displayed as TODO

--- source/core/3d/Material.hpp
+++ source/core/3d/Material.hpp
@@ -20,7 +20,7 @@ struct IObserver {
   virtual void update(Material* mat) {}
 };
 struct Polygon;
-struct Material {
+struct Material : public virtual kpi::IObject {
   virtual ~Material() = default;
 
   virtual std::string getName() const { return "Untitled Material"; }
