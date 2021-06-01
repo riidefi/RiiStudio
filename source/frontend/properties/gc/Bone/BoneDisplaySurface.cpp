6c6bfc00493be01f5b577e13af94b238bc95be32
riidefi <34194588+riidefi@users.noreply.github.com>
2021-06-01 12:00:54 -0600

    :sparkles: Make materials unique button + name materials by polygons button

--- source/frontend/properties/gc/Bone/BoneDisplaySurface.cpp
+++ source/frontend/properties/gc/Bone/BoneDisplaySurface.cpp
@@ -1,6 +1,7 @@
 #include <core/kpi/PropertyView.hpp>
 #include <core/util/gui.hpp>
 #include <librii/g3d/data/BoneData.hpp>
+#include <plugins/g3d/material.hpp>
 #include <plugins/gc/Export/Bone.hpp>
 #include <plugins/gc/Export/IndexedPolygon.hpp>
 #include <plugins/gc/Export/Material.hpp>
@@ -23,10 +24,76 @@ auto BoneDisplaySurface =
           if (ImGui::Button("Sort")) {
             auto* gb = dynamic_cast<librii::g3d::BoneData*>(&bone);
             if (gb) {
-              std::sort(gb->mDisplayCommands.begin(),
-                        gb->mDisplayCommands.end(),
-                        [](auto& l, auto& r) { return l.mPrio < r.mPrio; });
+              std::stable_sort(
+                  gb->mDisplayCommands.begin(), gb->mDisplayCommands.end(),
+                  [](auto& l, auto& r) { return l.mPrio < r.mPrio; });
             }
+            delegate.commit("Sorted");
+          }
+
+          if (ImGui::Button("Make materials unique")) {
+            auto* gb = dynamic_cast<librii::g3d::BoneData*>(&bone);
+            if (gb) {
+              riistudio::lib3d::Model* pMdl =
+                  dynamic_cast<riistudio::lib3d::Model*>(
+                      dynamic_cast<kpi::IObject*>(&bone)->childOf);
+              auto materials = pMdl->getMaterials();
+              std::map<u32, std::vector<u32>>
+                  mat_to_poly; // mat -> poly, poly, ...
+
+              for (auto& cmd : gb->mDisplayCommands) {
+                mat_to_poly[cmd.mMaterial].push_back(cmd.mPoly);
+              }
+
+              std::map<std::pair<u32, u32>, u32> remap; // [mat, poly] -> mat
+              for (auto& [mat, polys] : mat_to_poly) {
+                if (polys.size() <= 1)
+                  continue;
+                // Clone the material n times, assign to each
+                for (int i = 1; i < polys.size(); ++i) {
+                  remap[{mat, polys[i]}] = materials.size();
+
+                  auto* dst =
+                      dynamic_cast<riistudio::g3d::Material*>(&materials.add());
+                  assert(dst);
+                  auto* src =
+                      dynamic_cast<riistudio::g3d::Material*>(&materials[mat]);
+                  assert(src);
+
+                  riistudio::g3d::Material& created = *dst;
+                  *dst = *src;
+                  created.setName(created.getName() + "#" + std::to_string(i));
+                }
+              }
+              for (auto& cmd : gb->mDisplayCommands) {
+                auto key = std::pair<u32, u32>{cmd.mMaterial, cmd.mPoly};
+
+                auto found = remap.find(key);
+                if (found != remap.end()) {
+                  cmd.mMaterial = found->second;
+                }
+              }
+            }
+            delegate.commit("Material Unique Pass");
+          }
+          if (ImGui::Button("Name Materials By Polygons")) {
+            auto* gb = dynamic_cast<librii::g3d::BoneData*>(&bone);
+            if (gb) {
+              riistudio::lib3d::Model* pMdl =
+                  dynamic_cast<riistudio::lib3d::Model*>(
+                      dynamic_cast<kpi::IObject*>(&bone)->childOf);
+              auto materials = pMdl->getMaterials();
+              auto meshes = pMdl->getMeshes();
+
+              int i = 0;
+              for (auto& cmd : gb->mDisplayCommands) {
+                materials[cmd.mMaterial].setName(meshes[cmd.mPoly].getName() +
+                                                 "(Mat #" + std::to_string(i) +
+                                                 ")");
+                ++i;
+              }
+            }
+            delegate.commit("Name Materials By Polygons");
           }
 
           auto folder_id_combo = [](const char* title, const auto& folder,
