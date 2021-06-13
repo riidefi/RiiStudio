8dc2b894a9106f3845dbd798e2f28c0b5cf4e3a8
riidefi <34194588+riidefi@users.noreply.github.com>
2021-06-13 07:59:25 -0600

    :construction: Start SRT0 writing

--- source/librii/g3d/io/AnimIO.cpp
+++ source/librii/g3d/io/AnimIO.cpp
@@ -8,14 +8,29 @@
 
 namespace librii::g3d {
 
+#pragma region Key Frame : Fixed
+
+constexpr u32 KeyFrameBinSize = 12;
+
 static void ReadKeyFrame(KeyFrame& out, std::span<const u8> data) {
-  assert(data.size_bytes() >= 12);
+  assert(data.size_bytes() >= KeyFrameBinSize);
 
   out.frame = rsl::load<f32>(data, 0);
   out.value = rsl::load<f32>(data, 4);
   out.slope = rsl::load<f32>(data, 8);
 }
 
+static void WriteKeyFrame(std::span<u8> data, const KeyFrame& in) {
+  assert(data.size_bytes() == KeyFrameBinSize);
+
+  rsl::store<f32>(in.frame, data, 0);
+  rsl::store<f32>(in.value, data, 4);
+  rsl::store<f32>(in.slope, data, 8);
+}
+
+#pragma endregion
+
+#pragma region Key Frame Collection : Variable
 static bool ReadKeyFrameCollection(KeyFrameCollection& out,
                                    std::span<const u8> data) {
   constexpr u32 header_size = 8;
@@ -23,7 +38,7 @@ static bool ReadKeyFrameCollection(KeyFrameCollection& out,
     return false;
 
   const u16 keyframe_count = rsl::load<u16>(data, 0);
-  const u32 body_size = keyframe_count * 12;
+  const u32 body_size = keyframe_count * KeyFrameBinSize;
 
   out.step = rsl::load<f32>(data, 4);
 
@@ -34,7 +49,8 @@ static bool ReadKeyFrameCollection(KeyFrameCollection& out,
   out.data.resize(keyframe_count);
 
   for (size_t i = 0; i < out.data.size(); ++i) {
-    ReadKeyFrame(out.data[i], data.subspan(header_size + i * 12, 12));
+    ReadKeyFrame(out.data[i], data.subspan(header_size + i * KeyFrameBinSize,
+                                           KeyFrameBinSize));
   }
 
   // TODO: Warn if out.step != CalcStep
@@ -42,12 +58,43 @@ static bool ReadKeyFrameCollection(KeyFrameCollection& out,
   return true;
 }
 
+static u32 CalcKeyFrameCollectionSize(u32 num_keys) {
+  constexpr u32 header_size = 8;
+
+  return header_size + KeyFrameBinSize * num_keys;
+}
+
+static void WriteKeyFrameCollection(std::span<u8> data,
+                                    const KeyFrameCollection& in) {
+  assert(data.size_bytes() >= CalcKeyFrameCollectionSize(in.data.size()));
+  rsl::store<u16>(in.data.size(), data, 0);
+  rsl::store<u16>(0, data, 2); // Padding
+  rsl::store<f32>(in.step, data, 4);
+
+  for (auto it = in.data.begin(); it != in.data.end(); ++it) {
+    const size_t index = std::distance(in.data.begin(), it);
+
+    WriteKeyFrame(data.subspan(8 + index * KeyFrameBinSize, KeyFrameBinSize),
+                  *it);
+  }
+}
+#pragma endregion
+
+#pragma region Temporal Wrap Mode : Fixed
 static AnimationWrapMode ReadAnimationWrapMode(std::span<const u8> data) {
   assert(data.size_bytes() >= 4);
 
   return static_cast<AnimationWrapMode>(rsl::load<u32>(data, 0));
 }
 
+static void WriteAnimationWrapMode(std::span<u8> data, AnimationWrapMode mode) {
+  assert(data.size_bytes() >= 4);
+
+  rsl::store<u32>(static_cast<u32>(mode), data, 0);
+}
+#pragma endregion
+
+#pragma region SRT Control : Fixed
 struct SrtControl {
   u16 frame_duration;
   u16 material_count;
@@ -66,6 +113,18 @@ static SrtControl ReadSrtControl(std::span<const u8> data) {
                     .wrap_mode = ReadAnimationWrapMode(data.subspan(8))};
 }
 
+static void WriteSrtControl(std::span<u8> data, const SrtControl& control) {
+  assert(data.size_bytes() >= SrtControlBinSize);
+
+  rsl::store<u16>(control.frame_duration, data, 0);
+  rsl::store<u16>(control.material_count, data, 2);
+
+  rsl::store<u32>(control.xform_model, data, 4);
+  WriteAnimationWrapMode(data.subspan(8), control.wrap_mode);
+}
+#pragma endregion
+
+#pragma region Raw SRT Flag Bitfield : Data
 enum class SrtTexFlagVals {
   Unknown,
   ScaleOne,
@@ -85,6 +144,14 @@ static bool TestSrtTexFlag(u32 flags, SrtTexFlagVals val) {
   return flags & flag_bit;
 }
 
+static u32 SetSrtTexFlag(u32 flags, SrtTexFlagVals val) {
+  const u32 flag_bit = 1 << static_cast<u32>(val);
+
+  return flags | flag_bit;
+}
+#pragma endregion
+
+#pragma region Raw SRT Tex Header
 struct SrtTexDataHeader {
   u32 flags; // SrtTexFlagVals
 };
@@ -97,6 +164,17 @@ static SrtTexDataHeader ReadSrtTexDataHeader(std::span<const u8> data) {
   return SrtTexDataHeader{.flags = rsl::load<u32>(data, 0)};
 }
 
+static void WriteSrtTexDataHeader(std::span<u8> data,
+                                  const SrtTexDataHeader& header) {
+  assert(data.size_bytes() >= SrtTexDataHeaderBinSize);
+
+  rsl::store<u32>(header.flags, data, 0);
+}
+#pragma endregion
+
+// TODO: Need writing
+
+#pragma region Decomposed SRT Tex Header : Fixed
 SrtFlags ConvertToFlags(const SrtTexDataHeader& header) {
   return SrtFlags{
       .Unknown = TestSrtTexFlag(header.flags, SrtTexFlagVals::Unknown),
@@ -105,6 +183,23 @@ SrtFlags ConvertToFlags(const SrtTexDataHeader& header) {
       .TransZero = TestSrtTexFlag(header.flags, SrtTexFlagVals::TransZero),
       .ScaleUniform =
           TestSrtTexFlag(header.flags, SrtTexFlagVals::ScaleIsotropic)};
+}
+
+SrtTexDataHeader ConvertFromFlags(const SrtFlags& flags) {
+  u32 result = 0;
+
+  if (flags.Unknown)
+    result = SetSrtTexFlag(result, SrtTexFlagVals::Unknown);
+  if (flags.ScaleOne)
+    result = SetSrtTexFlag(result, SrtTexFlagVals::ScaleOne);
+  if (flags.RotZero)
+    result = SetSrtTexFlag(result, SrtTexFlagVals::RotZero);
+  if (flags.TransZero)
+    result = SetSrtTexFlag(result, SrtTexFlagVals::TransZero);
+  if (flags.ScaleUniform)
+    result = SetSrtTexFlag(result, SrtTexFlagVals::ScaleIsotropic);
+
+  return SrtTexDataHeader{.flags = result};
 }
 
 std::array<bool, NumberOfSrtAttributes>
@@ -120,27 +215,72 @@ struct SrtTexData {
   SrtFlags flags;
   std::array<bool, NumberOfSrtAttributes>
       attributes_fixed; // True = fixed, false = animated
-  std::array<std::optional<s32>, NumberOfSrtAttributes> anim_handles_offsets;
 };
 
-static std::optional<SrtTexData> ReadSrtTexData(std::span<const u8> whole_data,
-                                                unsigned offset) {
+std::optional<SrtTexData> ReadSrtTexDataFixed(std::span<const u8> whole_data,
+                                              unsigned offset) {
   if (whole_data.subspan(offset).size_bytes() < SrtTexDataHeaderBinSize) {
     return std::nullopt;
   }
 
   const auto header = ReadSrtTexDataHeader(whole_data.subspan(offset));
 
-  SrtTexData result{.flags = ConvertToFlags(header),
+  return SrtTexData{.flags = ConvertToFlags(header),
                     .attributes_fixed = ConvertToAttrArray(header)};
+}
+#pragma endregion
 
+// TODO: Need writing
+#pragma region SRT Tex Header + Data pointers : Variable
+struct SrtTexDataEx : public SrtTexData {
+  SrtTexData data;
+  std::array<std::optional<s32>, NumberOfSrtAttributes> anim_handles_offsets;
+};
+
+static u32 CalcNumSrtTexDataAnimProperties(const SrtFlags& flags) {
+  u32 counter = 0;
+
+  for (int i = 0; i < NumberOfSrtAttributes; ++i) {
+    const SrtAttribute attrib = static_cast<SrtAttribute>(i);
+
+    if (!IsSrtAttributeIncluded(flags, attrib))
+      ++counter;
+  }
+
+  return counter;
+}
+
+static u32 CalcSrtTexDataSize(const SrtTexDataEx& data) {
+  return SrtTexDataHeaderBinSize +
+         sizeof(s32) * CalcNumSrtTexDataAnimProperties(data.flags);
+}
+
+static std::optional<SrtTexDataEx>
+ReadSrtTexData(std::span<const u8> whole_data, unsigned offset) {
+  const auto header = ReadSrtTexDataFixed(whole_data, offset);
+  if (!header.has_value()) {
+    return std::nullopt;
+  }
+
+  if (whole_data.subspan(offset).size_bytes() <
+      CalcNumSrtTexDataAnimProperties(header->flags)) {
+    return std::nullopt;
+  }
+
+  SrtTexDataEx result;
+  result.data = *header;
+
+  // TODO: Make own function
   unsigned cursor = offset + SrtTexDataHeaderBinSize;
 
   for (int i = 0; i < NumberOfSrtAttributes; ++i) {
     const SrtAttribute attrib = static_cast<SrtAttribute>(i);
 
-    if (!IsSrtAttributeIncluded(result.flags, attrib))
+    if (!IsSrtAttributeIncluded(header->flags, attrib))
       continue;
+
+    if (cursor >= whole_data.size_bytes())
+      return std::nullopt;
 
     result.anim_handles_offsets[i] = cursor;
     cursor += 4;
@@ -149,6 +289,10 @@ static std::optional<SrtTexData> ReadSrtTexData(std::span<const u8> whole_data,
   return result;
 }
 
+#pragma endregion
+
+// TODO: Need writing
+#pragma region SRT Header : Fixed
 struct SrtHeader {
   CommonHeader common;
   s32 ofs_srt_dict;
@@ -184,6 +328,10 @@ static std::optional<SrtHeader> ReadSrtHeader(std::span<const u8> data) {
       .control = ReadSrtControl(data.subspan(CommonHeaderBinSize + 16))};
 }
 
+#pragma endregion
+
+// TODO: Need writing
+#pragma region SRT Material Header : Fixed
 struct SrtMatDataHeader {
   std::string_view material_name;
   u32 enabled_texsrts;
@@ -220,13 +368,20 @@ constexpr size_t CalcNumAnimations(const SrtMatDataHeader& header) {
 
   return total;
 }
+#pragma endregion
 
+// TODO: Need writing
+#pragma region SRT Material Data : Variable
 struct SrtMatData {
   std::string_view material_name;
 
   std::array<std::optional<s32>, 8> abs_tex_ofs;
   std::array<std::optional<s32>, 3> abs_ind_ofs;
 };
+
+static u32 CalcSrtMatDataSize(const SrtMatDataHeader& header) {
+  return SrtMatDataHeaderBinSize + CalcNumAnimations(header) * sizeof(s32);
+}
 
 std::optional<SrtMatData> ReadSrtMatData(std::span<const u8> whole_data,
                                          size_t offset) {
@@ -237,10 +392,8 @@ std::optional<SrtMatData> ReadSrtMatData(std::span<const u8> whole_data,
   }
 
   const auto header = ReadSrtMatDataHeader(data);
-  const u32 required_bytes =
-      SrtMatDataHeaderBinSize + CalcNumAnimations(header) * 4;
 
-  if (data.size_bytes() < required_bytes) {
+  if (data.size_bytes() < CalcSrtMatDataSize(header)) {
     return std::nullopt;
   }
 
@@ -262,6 +415,11 @@ std::optional<SrtMatData> ReadSrtMatData(std::span<const u8> whole_data,
   return result;
 }
 
+#pragma endregion
+
+// TODO: Need writing
+// TODO: Need size calc
+#pragma region Texture Anim : Data->(Fixed | Variable)
 static std::optional<SrtMatrixAnimation>
 ReadTexSrtAnimation(std::span<const u8> data, unsigned texsrt_offset) {
   auto tex_desc = ReadSrtTexData(data, texsrt_offset);
@@ -295,7 +453,10 @@ ReadTexSrtAnimation(std::span<const u8> data, unsigned texsrt_offset) {
 
   return tex_out;
 }
+#pragma endregion
 
+// TODO: Need writing
+// TODO: Need size calc
 bool ReadSrtFile(SrtAnimationArchive& arc, std::span<const u8> data) {
   const auto header = ReadSrtHeader(data);
   if (!header.has_value()) {
