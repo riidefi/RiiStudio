a9f4512d1e91f122d428b87e18c1c6ade728fcbe
riidefi <34194588+riidefi@users.noreply.github.com>
2021-07-10 07:07:37 -0600

    :bug: Don't shuffle windows when titles change (changing language)

--- source/frontend/editor/StudioWindow.cpp
+++ source/frontend/editor/StudioWindow.cpp
@@ -9,24 +9,48 @@ StudioWindow::StudioWindow(const std::string& name, bool dockspace)
   mWindowClass.ClassId = mId;
 }
 
-std::string StudioWindow::idIfy(std::string in) {
-  std::string out = std::string(riistudio::translateString(in)) + "##";
+std::string GetInternalIDOfWindow(std::string win_title,
+                                  std::string win_parent_title = "") {
+  return win_title + " of " + win_parent_title;
+}
+
+std::string TagWindowNameWithInternalID(std::string win_title,
+                                        std::string internal_id) {
+  return win_title + "###" + internal_id;
+}
+
+std::string StudioWindow::idIfy(std::string english_title_of_self) {
+  std::string win_title = english_title_of_self;
+  std::string win_parent_title = "";
+
+  // TODO: Deeper layers?
   if (getParent() != nullptr &&
       dynamic_cast<StudioWindow*>(getParent()) != nullptr) {
-    out += reinterpret_cast<StudioWindow*>(getParent())->mName;
+    win_parent_title = reinterpret_cast<StudioWindow*>(getParent())->mName;
   }
-  return out;
+
+  std::string internal_id = GetInternalIDOfWindow(win_title, win_parent_title);
+
+  return TagWindowNameWithInternalID(
+      riistudio::translateString(english_title_of_self), internal_id);
 }
 std::string StudioWindow::idIfyChild(std::string title) {
-  return std::string(riistudio::translateString(title)) + "##" + mName;
+  return TagWindowNameWithInternalID(title,
+                                     GetInternalIDOfWindow(title, mName));
 }
 void StudioWindow::draw() {
+  // Inherit window class (only can dock children into a parent)
   StudioWindow* parent = dynamic_cast<StudioWindow*>(getParent());
   if (parent)
     ImGui::SetNextWindowClass(parent->getWindowClass());
+
+  // Set window size to 1/4th of parent region
   auto avail = ImGui::GetContentRegionAvail();
   ImGui::SetNextWindowSize(ImVec2{avail.x / 2, avail.y / 2}, ImGuiCond_Once);
-  if (ImGui::Begin(idIfy(mName).c_str(), getOpen(), mFlags)) {
+
+  // The window name serves as the ID, use ### to ignore translated component
+  const auto id_name = idIfy(mName);
+  if (ImGui::Begin(id_name.c_str(), getOpen(), mFlags)) {
     ImGui::PushID(mName.c_str() /*mId*/);
 
     if (mbDrawDockspace) {
