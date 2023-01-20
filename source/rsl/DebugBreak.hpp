19c5fabe9e9bf1b5c4367682d103565576d47dc8
riidefi <34194588+riidefi@users.noreply.github.com>
2023-01-20 09:55:33 -0600

    Implement debug_break on MacOS/Linux

--- source/rsl/DebugBreak.hpp
+++ source/rsl/DebugBreak.hpp
@@ -1,10 +1,16 @@
 #pragma once
 
+#ifndef _WIN32
+#include <signal.h>
+#endif
+
 namespace rsl {
 
 static inline void debug_break() {
 #ifdef _WIN32
   __debugbreak();
+#else
+  raise(SIGTRAP);
 #endif
 }
 
