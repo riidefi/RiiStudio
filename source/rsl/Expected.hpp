a5e17b3614d7426c015078d8f032edaa716135d8
riidefi <34194588+riidefi@users.noreply.github.com>
2023-07-05 13:01:39 -0600

    [Apple] Use `tl::expected` in all cases
    The builtin one from Homebrew `clang` 16.0.6 was causing many crashes.

--- source/rsl/Expected.hpp
+++ source/rsl/Expected.hpp
@@ -1,16 +1,10 @@
 #pragma once
 
-// As AppleClang doesn't support C++23 std::expected yet.
-#if __cpp_lib_expected >= 202202L
-#include <expected>
-#else
 #ifdef __APPLE__
 #include <tl/expected.hpp>
 namespace std {
-  using namespace tl;
+    using namespace tl;
 }
-#define __cpp_lib_expected 202202L
 #else
-#error "Unsupported compiler version: must support std::expected"
-#endif
+#include <expected>
 #endif
