e91dfa9c3fa55e61959c59ef6d6b272f30f1c667
riidefi <34194588+riidefi@users.noreply.github.com>
2023-07-12 23:38:54 -0600

    [tests] Don't use legacy plugins backend

--- source/tests/tests.cpp
+++ source/tests/tests.cpp
@@ -4,7 +4,8 @@
 #include <librii/egg/LTEX.hpp>
 #include <librii/egg/PBLM.hpp>
 #include <librii/kmp/io/KMP.hpp>
-#include <plugins/api.hpp>
+#include <plugins/g3d/G3dIo.hpp>
+#include <plugins/j3d/J3dIo.hpp>
 #include <rsl/InitLLVM.hpp>
 #include <rsl/Ranges.hpp>
 
@@ -20,103 +21,6 @@ namespace llvm {
 int DisableABIBreakingChecks;
 } // namespace llvm
 
-void save(std::string_view path, kpi::INode& root,
-          std::vector<u8>* data = nullptr, std::span<const u32> bps = {}) {
-  printf("Writing to %s\n", std::string(path).c_str());
-  oishii::Writer writer(std::endian::big);
-
-  if (data != nullptr) {
-    writer.attachDataForMatchingOutput(*data);
-  }
-  for (u32 bp : bps) {
-    writer.add_bp<u32>(bp);
-  }
-
-  auto ex = SpawnExporter(root);
-  auto ok = ex->write_(root, writer);
-  if (!ok) {
-    fprintf(stderr, "Error writing file: %s\n", ok.error().c_str());
-    return;
-  }
-
-  writer.saveToDisk(path);
-}
-
-std::optional<std::pair<std::unique_ptr<kpi::INode>, std::vector<u8>>>
-open(std::string path, std::span<const u32> bps = {}) {
-  auto file = OishiiReadFile2(path);
-  if (!file.has_value()) {
-    std::cout << "Failed to read file!\n";
-    return std::nullopt;
-  }
-
-  auto importer = SpawnImporter(std::string(path), *file);
-
-  if (!importer.second) {
-    printf("Cannot spawn importer..\n");
-    return std::nullopt;
-  }
-  if (!IsConstructible(importer.first)) {
-    printf("Non constructable state.. find parents\n");
-
-    const auto children = GetChildrenOfType(importer.first);
-    if (children.empty()) {
-      printf("No children. Cannot construct.\n");
-      return std::nullopt;
-    }
-    assert(/*children.size() == 1 &&*/ IsConstructible(children[0])); // TODO
-    importer.first = children[0];
-  }
-
-  std::unique_ptr<kpi::INode> fileState{
-      dynamic_cast<kpi::INode*>(SpawnState(importer.first).release())};
-  if (!fileState.get()) {
-    printf("Cannot spawn file state %s.\n", importer.first.c_str());
-    return std::nullopt;
-  }
-  std::vector<std::string> logs;
-  const auto add_log = [&logs](kpi::IOMessageClass message_class,
-                               const std::string_view domain,
-                               const std::string_view message_body) {
-    logs.push_back(std::format("{}: {} (in {})\n",
-                               magic_enum::enum_name(message_class),
-                               message_body, domain));
-  };
-  kpi::IOTransaction transaction{
-      {
-          add_log,
-          kpi::TransactionState::Complete,
-      },
-      *fileState,
-      *file,
-      path,
-  };
-  for (u32 bp : bps) {
-    importer.second->addBp(bp);
-  }
-  do {
-    if (transaction.state == kpi::TransactionState::ResolveDependencies) {
-      fprintf(stderr, "Cannot resolve dependencies in test mode.\n");
-      return std::nullopt;
-    }
-    if (transaction.state == kpi::TransactionState::ConfigureProperties) {
-      transaction.state = kpi::TransactionState::Complete;
-    }
-
-    importer.second->read_(transaction);
-  } while (transaction.state != kpi::TransactionState::Failure &&
-           transaction.state != kpi::TransactionState::FailureToSave &&
-           transaction.state != kpi::TransactionState::Complete);
-
-  {
-    for (auto& log : logs) {
-      fprintf(stderr, "%s", log.c_str());
-    }
-  }
-
-  return std::make_pair(std::move(fileState), *file | rsl::ToList());
-}
-
 // XXX: Hack, though we'll refactor all of this way soon
 extern std::string rebuild_dest;
 
@@ -126,7 +30,8 @@ void rebuild(std::string from, const std::string_view to, bool check,
 
   if (from.ends_with("kmp") || from.ends_with("blight") ||
       from.ends_with("blmap") || from.ends_with("bdof") ||
-      from.ends_with("bblm")) {
+      from.ends_with("bblm") || from.ends_with("bmd") ||
+      from.ends_with("bdl") || from.ends_with("brres")) {
     auto file = OishiiReadFile2(from);
     if (!file.has_value()) {
       printf("Cannot rebuild\n");
@@ -198,24 +103,58 @@ void rebuild(std::string from, const std::string_view to, bool check,
       auto bdof2 = librii::egg::To_PBLM(*dof);
       printf("Writing to %s\n", std::string(to).c_str());
       librii::egg::PBLM_Write(writer, bdof2);
+    } else if (from.ends_with("brres")) {
+      // writer.attachDataForMatchingOutput(*file | rsl::ToList());
+      riistudio::g3d::Collection brres;
+      kpi::LightIOTransaction trans;
+      trans.callback = [&](kpi::IOMessageClass message_class,
+                           const std::string_view domain,
+                           const std::string_view message_body) {
+        auto msg =
+            std::format("[{}] {} {}", magic_enum::enum_name(message_class),
+                        domain, message_body);
+        rsl::error(msg);
+      };
+      riistudio::g3d::ReadBRRES(brres, reader, trans);
+      if (trans.state != kpi::TransactionState::Complete) {
+        fprintf(stderr, "Failed to read BRRES\n");
+        return;
+      }
+      printf("Writing to %s\n", std::string(to).c_str());
+      auto bruh = riistudio::g3d::WriteBRRES(brres, writer);
+      if (!bruh) {
+        fprintf(stderr, "Failed to write BRRES: %s\n", bruh.error().c_str());
+        return;
+      }
+    } else if (from.ends_with("bmd") || from.ends_with("bdl")) {
+      // writer.attachDataForMatchingOutput(*file | rsl::ToList());
+      riistudio::j3d::Collection bmd;
+      kpi::LightIOTransaction trans;
+      trans.callback = [&](kpi::IOMessageClass message_class,
+                           const std::string_view domain,
+                           const std::string_view message_body) {
+        auto msg =
+            std::format("[{}] {} {}", magic_enum::enum_name(message_class),
+                        domain, message_body);
+        rsl::error(msg);
+      };
+
+      if (auto ok = riistudio::j3d::ReadBMD(bmd, reader, trans); !ok) {
+        fprintf(stderr, "Failed to read BMD/BDL: %s\n", ok.error().c_str());
+        return;
+      }
+      printf("Writing to %s\n", std::string(to).c_str());
+      auto bruh = riistudio::j3d::WriteBMD(bmd, writer);
+      if (!bruh) {
+        fprintf(stderr, "Failed to write BMD/BDL: %s\n", bruh.error().c_str());
+        return;
+      }
     }
     writer.saveToDisk(to);
     return;
   }
-
-  auto result =
-      open(from, bps | std::views::filter([](auto& x) { return x < 0; }) |
-                     std::views::transform([](auto x) -> u32 { return -x; }) |
-                     rsl::ToList());
-  if (!result) {
-    printf("Cannot rebuild!\n");
-    return;
-  }
-  auto& [data, raw] = *result;
-  save(to, *data, check ? &raw : nullptr,
-       bps | std::views::filter([](auto& x) { return x > 0; }) |
-           std::views::transform([](auto x) -> u32 { return x; }) |
-           rsl::ToList());
+  fprintf(stderr, "Unrecognized format; Failed to rebuild\n");
+  return;
 }
 
 extern bool gTestMode;
@@ -228,9 +167,6 @@ int main(int argc, const char** argv) {
   ANNOUNCE("Initializing LLVM");
   rsl::InitLLVM init_llvm(argc, argv);
 
-  ANNOUNCE("Initializing plugins");
-  InitAPI();
-
   ANNOUNCE("Performing tasks");
   if (argc < 3) {
     fprintf(stderr,
@@ -240,9 +176,9 @@ int main(int argc, const char** argv) {
     for (int i = 4; i < argc; ++i) {
       bps.push_back(std::stoi(argv[i], nullptr, 16));
     }
-    rebuild(argv[1], argv[2], argc > 3 && !strcmp(argv[3], "check"), bps);
+    bool check = argc > 3 && !strcmp(argv[3], "check");
+    rebuild(argv[1], argv[2], check, bps);
   }
 
   ANNOUNCE("Done!");
-  DeinitAPI();
 }
