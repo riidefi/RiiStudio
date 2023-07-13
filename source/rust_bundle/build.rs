ff152e17f56448b9c9041d0375568ec66f89e01e
riidefi <34194588+riidefi@users.noreply.github.com>
2023-07-12 22:36:13 -0600

    [Apple] WIP bundle as .app

--- source/rust_bundle/build.rs
+++ source/rust_bundle/build.rs
@@ -1,5 +1,22 @@
-use std::process::Command;
+use std::fs;
 use std::path::Path;
+
+#[cfg(windows)]
+use std::process::Command;
+
+
+fn copy_dir<P: AsRef<Path>, Q: AsRef<Path>>(src: P, dst: Q) -> std::io::Result<()> {
+    for entry in fs::read_dir(src)? {
+        let entry = entry?;
+        let path = entry.path();
+        if entry.file_type()?.is_dir() {
+            copy_dir(&path, &dst.as_ref().join(path.file_name().unwrap()))?;
+        } else {
+            fs::copy(&path, &dst.as_ref().join(path.file_name().unwrap()))?;
+        }
+    }
+    Ok(())
+}
 
 fn link_lib(directory: &str, lib: &str) {
     println!("cargo:rustc-link-search=native={}", directory);
@@ -7,11 +24,36 @@ fn link_lib(directory: &str, lib: &str) {
 }
 
 fn main() {
+    #[cfg(windows)]
     let build_dir = "..\\..\\out\\build\\Clang-x64-DIST\\";
+    #[cfg(unix)]
+    let build_dir = "../../build/";
+    #[cfg(windows)]
     let source_dir = "..\\..\\source\\";
-    let out_dir = format!("{}source\\", build_dir);
-    let deps_dir = format!("{}_deps\\", build_dir);
+    #[cfg(unix)]
+    let source_dir = "../../source/";
+
+    let out_dir = format!("{}source/", build_dir);
+    let deps_dir = format!("{}_deps/", build_dir);
+
+    #[cfg(windows)]
     let cargo_dir = format!("{}cargo\\build\\x86_64-pc-windows-msvc\\release\\", build_dir);
+    #[cfg(unix)]
+    let cargo_dir = format!("{}cargo/build/aarch64-apple-darwin/debug/", build_dir);
+
+    #[cfg(unix)]
+    {
+        println!("cargo:rustc-link-search=native={}", "/opt/homebrew/Cellar/freetype/2.13.1/lib/");
+        println!("cargo:rustc-link-lib=static={}", "freetype");
+        println!("cargo:rustc-link-search=native={}", "/opt/homebrew/Cellar/glfw/3.3.8/lib/");
+        println!("cargo:rustc-link-lib=static={}", "glfw");
+        println!("cargo:rustc-link-search=native={}", "/opt/homebrew/Cellar/assimp/5.2.5/lib/");
+        println!("cargo:rustc-link-lib=static={}", "assimp");
+        println!("cargo:rustc-link-lib=static={}", "dl");
+        println!("cargo:rustc-link-lib=static={}", "c++");
+        println!("cargo:rustc-link-lib=framework={}", "CoreFoundation");
+    }
+ 
 
     let libs = vec![
         (format!("{}{}", out_dir, "frontend"), "lib_frontend"),
@@ -25,11 +67,8 @@ fn main() {
         (format!("{}{}", out_dir, "rsl"), "rsl"),
         (format!("{}{}", out_dir, "updater"), "updater"),
         (format!("{}{}", out_dir, "vendor"), "vendor"),
-        (format!("{}{}", source_dir, "vendor"), "freetype"),
-        (format!("{}{}", source_dir, "plate\\vendor\\glfw\\lib-vc2017"), "glfw3dll"),
-        (format!("{}{}", source_dir, "vendor\\assimp"), "assimp-vc141-mt"),
-        (format!("{}{}", deps_dir, "fmt-build"), "fmt"),
-        (format!("{}{}", deps_dir, "libfort-build\\lib"), "fort"),
+        (format!("{}{}", deps_dir, "fmt-build"), "fmtd"),
+        (format!("{}{}", deps_dir, "libfort-build/lib"), "fort"),
         (format!("{}{}", deps_dir, "meshoptimizer-build"), "meshoptimizer"),
     ];
 
@@ -40,6 +79,7 @@ fn main() {
     // gendef.exe llvm_sighandler.dll
     // llvm-dlltool.exe -D llvm_sighandler.dll -d llvm_sighandler.def -l llvm_sighandler.lib -m i386:x86-64
 
+    #[cfg(windows)]
     let dlls = vec!["gctex.dll", "riistudio_rs.dll", "llvm_sighandler.dll"];
 
     #[cfg(windows)]
@@ -86,4 +126,25 @@ fn main() {
         println!("cargo:rustc-link-lib=static=Ncrypt");
         println!("cargo:rustc-link-lib=static=Shell32");
     }
+
+    // Get the directory that the build script is running in.
+
+    // The source directories for fonts, lang, and icon.png.
+    let font_src = format!("{}{}", source_dir, "../fonts");
+    let lang_src = format!("{}{}", source_dir, "../lang");
+    let icon_src = format!("{}{}", source_dir, "frontend/rc/icon.png");
+
+    let out_dir = "target/debug";
+
+    // The destination directories for fonts, lang, and icon.png.
+    let font_dest = Path::new(&out_dir).join("fonts");
+    let lang_dest = Path::new(&out_dir).join("lang");
+    let icon_dest = Path::new(&out_dir).join("icon.png");
+
+    // Copy the directories and files.
+    fs::create_dir_all(&font_dest).unwrap();
+    fs::create_dir_all(&lang_dest).unwrap();
+    fs::copy(icon_src, &icon_dest).unwrap();
+    copy_dir(font_src, &font_dest).unwrap();
+    copy_dir(lang_src, &lang_dest).unwrap();
 }
