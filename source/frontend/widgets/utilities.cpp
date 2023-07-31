9a0827e313877ea6717da2bafe3c83cf86ec407f
JoshuaMK <60854312+JoshuaMKW@users.noreply.github.com>
2023-07-30 22:24:51 -0500

    Remove redundancy in utilities

--- source/frontend/widgets/utilities.cpp
+++ source/frontend/widgets/utilities.cpp
@@ -5,17 +5,7 @@ namespace riistudio::frontend {
 
 void DrawItemBorder(const ImVec4& color,
                     std::optional<ImRect> padding = std::nullopt) {
-  // Then, we get the bounding box of the last item
-  ImRect bb = {ImGui::GetItemRectMin(), ImGui::GetItemRectMax()};
-  if (padding) {
-    bb.Min -= padding->Min;
-    bb.Max += padding->Max;
-  }
-
-  // Then we use the ImGui draw list to draw a custom border
-  ImDrawList* draw_list = ImGui::GetWindowDrawList();
-  draw_list->AddRect(bb.Min, bb.Max,
-                     ImGui::ColorConvertFloat4ToU32(color)); // RGBA
+  DrawItemBorder(ImGui::ColorConvertFloat4ToU32(color), padding);
 }
 
 void DrawItemBorder(ImU32 color, std::optional<ImRect> padding = std::nullopt) {
