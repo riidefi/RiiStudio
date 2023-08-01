b68d3ca70851b5be4b21e90359d904846c9a92f0
riidefi <34194588+riidefi@users.noreply.github.com>
2023-08-01 00:16:30 -0600

    :bookmark: Bump version

--- source/core/util/timestamp.cpp
+++ source/core/util/timestamp.cpp
@@ -11,10 +11,10 @@
 #endif
 
 // Must match release on Github
-const char GIT_TAG[] = "Alpha 5.10.10 (Hotfix 3)";
+const char GIT_TAG[] = "Alpha 5.10.11";
 
 // Must be exactly 16 bytes long
-const char VERSION_SHORT[] = "RiiStudioA5.10.a";
+const char VERSION_SHORT[] = "RiiStudioA5.10.b";
 static_assert(sizeof(VERSION_SHORT) - 1 == 16,
               "VERSION_SHORT must be 16 bytes long");
 
@@ -23,7 +23,7 @@ static_assert(sizeof(VERSION_SHORT) - 1 == 16,
 #elif defined(BUILD_RELEASE)
 #define RII_BUILD "Alpha Release"
 #elif defined(BUILD_DIST)
-#define RII_BUILD "Alpha 5.10.10 (Hotfix 3)"
+#define RII_BUILD "Alpha 5.10.11"
 #else
 #define RII_BUILD "Custom"
 #endif
