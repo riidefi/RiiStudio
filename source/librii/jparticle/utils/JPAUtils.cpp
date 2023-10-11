d3f426209e8eb153a85cb430527b9b55db0ef705
CyrusHedgehog <cyrushedgehog@outlook.com>
2023-10-11 20:28:14 +0100

    Remove Unused Functions

--- source/librii/jparticle/utils/JPAUtils.cpp
+++ source/librii/jparticle/utils/JPAUtils.cpp
@@ -14,20 +14,4 @@ u16 JPAConvertFloatToFixed(f32 number) {
   return static_cast<short>(number * 0x8000);
 }
 
-f32* JPAConvertFixVectorToFloatVector(const u16 vector[3]) {
-  f32* converted = new f32[3];
-  converted[0] = JPAConvertFixToFloat(vector[0]);
-  converted[1] = JPAConvertFixToFloat(vector[1]);
-  converted[2] = JPAConvertFixToFloat(vector[2]);
-  return converted;
-}
-
-u16* JPAConvertFloatVectorToFixVector(const f32 vector[3]) {
-  u16* converted = new u16[3];
-  converted[0] = JPAConvertFloatToFixed(vector[0]);
-  converted[1] = JPAConvertFloatToFixed(vector[1]);
-  converted[2] = JPAConvertFloatToFixed(vector[2]);
-  return converted;
-}
-
 } // namespace librii::jpa
