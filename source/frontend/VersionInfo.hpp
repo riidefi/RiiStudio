f0e2ce3576e00b78ac96a4357fd43fd3ec2052e8
riidefi <34194588+riidefi@users.noreply.github.com>
2023-10-21 02:46:27 -0600

    [frontend] Add RS version to "Build Info" window

--- source/frontend/VersionInfo.hpp
+++ source/frontend/VersionInfo.hpp
@@ -1,16 +1,17 @@
 #pragma once
 
+#include <core/util/timestamp.hpp>
 #include <librii/image/ImagePlatform.hpp>
 #include <librii/szs/SZS.hpp>
 
 namespace riistudio::frontend {
 
 void DrawVersionInfo() {
+  ImGui::Text("%s", RII_TIME_STAMP);
   std::string gctex_ver =
       "gctex " + std::string(librii::image::gctex_version());
   ImGui::Text("%s", gctex_ver.c_str());
-  std::string szs_ver =
-      "szs " + std::string(librii::szs::szs_version());
+  std::string szs_ver = "szs " + std::string(librii::szs::szs_version());
   ImGui::Text("%s", szs_ver.c_str());
 }
 
