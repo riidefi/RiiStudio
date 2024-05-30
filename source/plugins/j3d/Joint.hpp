16d6306f5df988cc22c2482b1e84238f841a9fb1
riidefi <34194588+riidefi@users.noreply.github.com>
2024-05-30 17:26:09 -0600

    [frontend] j3d/Joint: Misc refactor

--- source/plugins/j3d/Joint.hpp
+++ source/plugins/j3d/Joint.hpp
@@ -10,50 +10,48 @@
 
 namespace riistudio::j3d {
 
-struct Joint : public libcube::IBoneDelegate,
-               private /* mutable */ librii::j3d::JointData,
-               public virtual kpi::IObject {
-  // PX_TYPE_INFO_EX("J3D Joint", "j3d_joint", "J::Joint", ICON_FA_BONE,
-  // ICON_FA_BONE);
-
-  std::string getName() const override { return name; }
-  void setName(const std::string& n) override { name = n; }
+class Joint : public libcube::IBoneDelegate, public virtual kpi::IObject {
+public:
+  std::string getName() const override { return m_jointData.name; }
+  void setName(const std::string& n) override { m_jointData.name = n; }
 
   librii::math::SRT3 getSRT() const override {
-    return {scale, rotate, translate};
+    return {m_jointData.scale, m_jointData.rotate, m_jointData.translate};
   }
   void setSRT(const librii::math::SRT3& srt) override {
-    scale = srt.scale;
-    rotate = srt.rotation;
-    translate = srt.translation;
+    m_jointData.scale = srt.scale;
+    m_jointData.rotate = srt.rotation;
+    m_jointData.translate = srt.translation;
   }
-  s64 getBoneParent() const override { return parentId; }
-  void setBoneParent(s64 id) override { parentId = (u32)id; }
-  u64 getNumChildren() const override { return children.size(); }
+  s64 getBoneParent() const override { return m_jointData.parentId; }
+  void setBoneParent(s64 id) override { m_jointData.parentId = (u32)id; }
+  u64 getNumChildren() const override { return m_jointData.children.size(); }
   s64 getChild(u64 idx) const override {
-    return idx < children.size() ? children[idx] : -1;
+    return idx < m_jointData.children.size() ? m_jointData.children[idx] : -1;
   }
   s64 addChild(s64 child) override {
-    children.push_back((u32)child);
-    return children.size() - 1;
+    m_jointData.children.push_back((u32)child);
+    return m_jointData.children.size() - 1;
   }
   s64 setChild(u64 idx, s64 id) override {
     s64 old = -1;
-    if (idx < children.size()) {
-      old = children[idx];
-      children[idx] = (u32)id;
+    if (idx < m_jointData.children.size()) {
+      old = m_jointData.children[idx];
+      m_jointData.children[idx] = (u32)id;
     }
     return old;
   }
-  librii::math::AABB getAABB() const override { return boundingBox; }
-  void setAABB(const librii::math::AABB& aabb) override { boundingBox = aabb; }
-  float getBoundingRadius() const override { return boundingSphereRadius; }
-  void setBoundingRadius(float r) override { boundingSphereRadius = r; }
-  bool getSSC() const override { return mayaSSC; }
-  void setSSC(bool b) override { mayaSSC = b; }
+  librii::math::AABB getAABB() const override { return j_boundingBox; }
+  void setAABB(const librii::math::AABB& aabb) override {
+    j_boundingBox = aabb;
+  }
+  float getBoundingRadius() const override { return j_boundingSphereRadius; }
+  void setBoundingRadius(float r) override { j_boundingSphereRadius = r; }
+  bool getSSC() const override { return j_mayaSSC; }
+  void setSSC(bool b) override { j_mayaSSC = b; }
 
   Billboard getBillboard() const override {
-    switch (bbMtxType) {
+    switch (m_jointData.bbMtxType) {
     case librii::j3d::MatrixType::Standard:
       return Billboard::None;
     case librii::j3d::MatrixType::Billboard:
@@ -67,51 +65,48 @@ struct Joint : public libcube::IBoneDelegate,
   void setBillboard(Billboard b) override {
     switch (b) {
     case Billboard::None:
-      bbMtxType = librii::j3d::MatrixType::Standard;
+      m_jointData.bbMtxType = librii::j3d::MatrixType::Standard;
       break;
     case Billboard::J3D_XY:
-      bbMtxType = librii::j3d::MatrixType::Billboard;
+      m_jointData.bbMtxType = librii::j3d::MatrixType::Billboard;
       break;
     case Billboard::J3D_Y:
-      bbMtxType = librii::j3d::MatrixType::BillboardY;
+      m_jointData.bbMtxType = librii::j3d::MatrixType::BillboardY;
       return;
     default:
       break;
     }
   }
-  u64 getNumDisplays() const override { return displays.size(); }
+  u64 getNumDisplays() const override { return m_jointData.displays.size(); }
   lib3d::Bone::Display getDisplay(u64 idx) const override {
-    return {(u32)displays[idx].material, (u32)displays[idx].shape};
+    return {(u32)m_jointData.displays[idx].material,
+            (u32)m_jointData.displays[idx].shape};
   }
   void setDisplay(u64 idx, const lib3d::Bone::Display& d) override {
-    auto& dst = displays[idx];
+    auto& dst = m_jointData.displays[idx];
     dst.material = d.matId;
     dst.shape = d.polyId;
   }
 
   void addDisplay(const lib3d::Bone::Display& d) override {
-    displays.emplace_back(d.matId, d.polyId);
+    m_jointData.displays.emplace_back(d.matId, d.polyId);
   }
 
   void decompile(const librii::j3d::JointData& b) {
-    static_cast<librii::j3d::JointData&>(*this) = b;
-    j_flag = flag;
-    j_bbMtxType = bbMtxType;
-    j_mayaSSC = mayaSSC;
-    j_boundingSphereRadius = boundingSphereRadius;
-    j_boundingBox = boundingBox;
+    m_jointData = b;
+    j_flag = m_jointData.flag;
+    j_bbMtxType = m_jointData.bbMtxType;
+    j_mayaSSC = m_jointData.mayaSSC;
+    j_boundingSphereRadius = m_jointData.boundingSphereRadius;
+    j_boundingBox = m_jointData.boundingBox;
   }
-  const librii::j3d::JointData& compile() {
-    flag = j_flag;
-    bbMtxType = j_bbMtxType;
-    mayaSSC = j_mayaSSC;
-    boundingSphereRadius = j_boundingSphereRadius;
-    boundingBox = j_boundingBox;
-    return *this;
-  }
-  const librii::j3d::JointData& compile() const {
-    // We treat the JointData parent as a mutable member
-    return const_cast<Joint*>(this)->compile();
+  const librii::j3d::JointData compile() const {
+    m_jointData.flag = j_flag;
+    m_jointData.bbMtxType = j_bbMtxType;
+    m_jointData.mayaSSC = j_mayaSSC;
+    m_jointData.boundingSphereRadius = j_boundingSphereRadius;
+    m_jointData.boundingBox = j_boundingBox;
+    return m_jointData;
   }
 
   u16 j_flag = 1; // Unused four bits; default value in galaxy is 1
@@ -122,13 +117,12 @@ struct Joint : public libcube::IBoneDelegate,
                                    {100000.0f, 100000.0f, 100000.0f}};
   bool operator==(const Joint& rhs) const {
     // We treat the JointData parent as a mutable member
-    const_cast<Joint*>(this)->compile();
-    return static_cast<const JointData&>(*this) == rhs &&
-           j_flag == rhs.j_flag && j_bbMtxType == rhs.j_bbMtxType &&
-           j_mayaSSC == rhs.j_mayaSSC &&
-           j_boundingSphereRadius == rhs.j_boundingSphereRadius &&
-           j_boundingBox == rhs.j_boundingBox;
+    compile();
+    return m_jointData == rhs.m_jointData;
   }
+
+private:
+  mutable librii::j3d::JointData m_jointData{};
 };
 
 } // namespace riistudio::j3d
