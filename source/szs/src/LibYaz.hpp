a12b1add0c488329f29749f6ca878f95eac9047c
riidefi <34194588+riidefi@users.noreply.github.com>
2024-07-10 14:25:51 -0700

    [szs] LibYaz: Make more rust-like

--- source/szs/src/LibYaz.hpp
+++ source/szs/src/LibYaz.hpp
@@ -6,15 +6,19 @@
 
 namespace rlibrii::szs {
 
-static inline void compressionSearch(const u8* src_pos, const u8* src,
-                                     int max_len, u32 search_range,
-                                     const u8* src_end, const u8** out_found,
-                                     u32* out_found_len) {
-  u32& found_len = *out_found_len;
-  const u8*& found = *out_found;
+struct SearchResult {
+  u32 found;
+  u32 found_len;
+};
 
-  found_len = 0;
-  found = nullptr;
+static inline SearchResult compressionSearch(u32 src_pos,
+                                             std::span<const u8> src_,
+                                             int max_len, u32 search_range) {
+  const u8* src = src_.data();
+  const u8* src_end = src + src_.size();
+  SearchResult result;
+  result.found_len = 0;
+  result.found = 0;
 
   const u8* search;
   const u8* cmp_end;
@@ -23,59 +27,59 @@ static inline void compressionSearch(const u8* src_pos, const u8* src,
   const u8* cmp2;
   int len_;
 
-  if (src_pos + 2 < src_end) {
-    search = src_pos - search_range;
+  if (src_pos + 2 < src_.size()) {
+    const u8* src_ptr = src + src_pos;
+    search = src_ptr - search_range;
     if (search < src)
       search = src;
 
-    cmp_end = src_pos + max_len;
+    cmp_end = src_ptr + max_len;
     if (cmp_end > src_end)
       cmp_end = src_end;
 
-    c1 = *src_pos;
-    while (search < src_pos) {
-      search = (u8*)memchr(search, c1, src_pos - search);
+    c1 = *src_ptr;
+    while (search < src_ptr) {
+      search = (u8*)memchr(search, c1, src_ptr - search);
       if (search == nullptr)
         break;
 
       cmp1 = search + 1;
-      cmp2 = src_pos + 1;
+      cmp2 = src_ptr + 1;
 
       while (cmp2 < cmp_end && *cmp1 == *cmp2) {
         cmp1++;
         cmp2++;
       }
 
-      len_ = cmp2 - src_pos;
+      len_ = static_cast<int>(cmp2 - src_ptr);
 
-      if (found_len < len_) {
-        found_len = len_;
-        found = search;
-        if (found_len == max_len)
+      if (result.found_len < static_cast<u32>(len_)) {
+        result.found_len = len_;
+        result.found = search - src;
+        if (result.found_len == max_len)
           break;
       }
 
       search++;
     }
   }
+
+  return result;
 }
 
-static inline void CompressYaz(const u8* src, u32 src_len, u8 level, u8* dst,
-                               u32 dst_len, u32* out_len) {
+static inline u32 CompressYaz_(const u8* src, u32 src_len, u8 level, u8* dst,
+                               u32 dst_len) {
+  (void)dst_len;
   writeU32(dst, 0x0, YAZ0_MAGIC);
   writeU32(dst, 0x4, src_len);
   writeU32(dst, 0x8, 0x0);
   writeU32(dst, 0xc, 0x0);
 
-  // TOOD: Check dest_len
   u32 search_range;
-
   if (level == 0)
     search_range = 0;
-
   else if (level < 9)
     search_range = 0x10e0 * level / 9 - 0x0e0;
-
   else
     search_range = 0x1000;
 
@@ -110,8 +114,11 @@ static inline void CompressYaz(const u8* src, u32 src_len, u8 level, u8* dst,
       src_pos += delta;
     }
   } else if (src_pos < src_end) {
-    compressionSearch(src_pos, src, max_len, search_range, src_end, &found,
-                      &found_len);
+    auto res =
+        compressionSearch(src_pos - src, std::span<const u8>(src, src_end),
+                          max_len, search_range);
+    found = src + res.found;
+    found_len = res.found_len;
 
     next_found = nullptr;
     next_found_len = 0;
@@ -124,9 +131,13 @@ static inline void CompressYaz(const u8* src, u32 src_len, u8 level, u8* dst,
         if (src_pos >= src_end)
           break;
 
-        if (src_pos + 1 < src_end)
-          compressionSearch(src_pos + 1, src, max_len, search_range, src_end,
-                            &next_found, &next_found_len);
+        if (src_pos + 1 < src_end) {
+          auto res_ = compressionSearch(src_pos - src + 1,
+                                        std::span<const u8>(src, src_end),
+                                        max_len, search_range);
+          next_found = src + res_.found;
+          next_found_len = res_.found_len;
+        }
 
         if (found_len > 2 && next_found_len <= found_len) {
           delta = src_pos - found - 1;
@@ -142,8 +153,11 @@ static inline void CompressYaz(const u8* src, u32 src_len, u8 level, u8* dst,
 
           src_pos += found_len;
 
-          compressionSearch(src_pos, src, max_len, search_range, src_end,
-                            &found, &found_len);
+          auto res_ = compressionSearch(src_pos - src,
+                                        std::span<const u8>(src, src_end),
+                                        max_len, search_range);
+          found = src + res_.found;
+          found_len = res_.found_len;
         } else {
           *code_byte |= 1 << i;
           *dst_pos++ = *src_pos++;
@@ -155,7 +169,13 @@ static inline void CompressYaz(const u8* src, u32 src_len, u8 level, u8* dst,
     }
   }
 
-  *out_len = dst_pos - dst;
+  u32 out_len = dst_pos - dst;
+  return out_len;
+}
+
+static inline void CompressYaz(const u8* src, u32 src_len, u8 level, u8* dst,
+                               u32 dst_len, u32* out_len) {
+  *out_len = CompressYaz_(src, src_len, level, dst, dst_len);
 }
 
 } // namespace rlibrii::szs
