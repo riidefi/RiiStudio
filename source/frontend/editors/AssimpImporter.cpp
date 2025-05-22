6f11828a2d7ce1af80aa91b6d4f0b30042cee906
riidefi <34194588+riidefi@users.noreply.github.com>
2025-05-22 09:28:22 -0700

    [frontend] AssimpImporter: Add in-app link to wiki docs

--- source/frontend/editors/AssimpImporter.cpp
+++ source/frontend/editors/AssimpImporter.cpp
@@ -8,6 +8,15 @@
 namespace riistudio::frontend {
 
 void AssimpEditorPropertyGrid::Draw(librii::assimp2rhst::Settings& ctx) {
+  if (ImGui::BeginChild("HelpBox", ImVec2(0, ImGui::GetTextLineHeightWithSpacing() * 1.5f), true, ImGuiWindowFlags_NoScrollbar)) {
+    ImGui::TextColored(ImVec4(0.4f, 0.8f, 1.0f, 1.0f), (const char*)ICON_FA_BOOK_OPEN);
+    ImGui::SameLine();
+    ImGui::Text("Documentation: ");
+    ImGui::SameLine();
+    ImGui::TextLinkOpenURL("https://wiki.tockdom.com/wiki/Creating_a_BRRES_with_RiiStudio#RiiStudio");
+  }
+  ImGui::EndChild();
+
   if (ImGui::CollapsingHeader("Importing Settings"_j,
                               ImGuiTreeNodeFlags_DefaultOpen)) {
     //
